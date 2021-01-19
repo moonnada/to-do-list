5be082b4b231b800c8da73ad73f15f9028fb9ddf
Byung Moon Kim <kimbm2014@gmail.com>
2021-01-19 10:22:20 -0800

    make a new file; "about" and apply header and footer

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food" , "Eat Food"];
+let workItem = [];
 
 app.set('view engine', 'ejs');  //this line of code mush be below of const app = express();
 
@@ -22,15 +23,37 @@ app.get("/", function(req,res){
 
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay : day , newListItems : items});
+    res.render("list", {listTitle : day , newListItems : items});
 });
 
 app.post("/", function(req,res){
+
+    // console.log(req.body);        //to check my key word
+    // let item = req.body.newItem;
+    // items.push(item);
+    // res.redirect("/");
+
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if(req.body.list === "Work"){
+        workItem.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItem });
+});
+
+app.get("/about", function(req,res){
+    res.render("about");
 })
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
-})
+});
