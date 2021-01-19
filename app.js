4a69e92dfcb40e31fa1cd3254fdd99c5bd896a3a
Byung Moon Kim <kimbm2014@gmail.com>
2021-01-19 05:38:18 -0800

    edit codes to make new list to origin list

--- app.js
+++ app.js
@@ -3,48 +3,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food" , "Eat Food"];
+
 app.set('view engine', 'ejs');  //this line of code mush be below of const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    if(currentDay === 6 || currentDay ===0){
-        day = "Weekend"; 
-    }
-    else{
-       day = "Weekday";
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
-    switch(currentDay) {
-        case 0 :
-            day = "Sunday";
-            break;
-        case 1 :
-            day = "Monday";
-            break;
-        case 0 :
-            day = "Tuesday";
-            break;
-        case 0 :
-            day = "Wednesday";
-            break;
-        case 0 :
-            day = "Thursday";
-            break;
-        case 0 :
-            day = "Friday";
-            break;
-        case 0 :
-            day = "Saturday";
-            break;   
-        default:
-            console.log ("Error the number can't be " +currentDay);     
-    }
-
-    res.render("list", {kindOfDay : day});
+    res.render("list", {kindOfDay : day , newListItems : items});
 });
+
+app.post("/", function(req,res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
